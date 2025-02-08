36eb89f9c931a27c259f21ff4a2c2ad13e5f0db6
twoneis <sanjay29@sapura.de>
2025-02-08 17:18:54 +0100

    initial iceshrimp package

--- modules/fedi/default.nix
+++ modules/fedi/default.nix
@@ -1,5 +1,8 @@
-{ lib, config, ... }: let
+{ lib, pkgs, config, ... }: let
   inherit (lib) mkIf;
   inherit (config) conf;
 in mkIf conf.fedi.enable {
+  environment.systemPackages = [
+    (pkgs.callPackage ./iceshrimp.nix { })
+  ];
 }
