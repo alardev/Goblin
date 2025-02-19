5940883d82c11929304b1f2ef1929ebdf2539478
twoneis <sanjay29@sapura.de>
2025-02-19 20:44:32 +0100

    don't mark broken anymore

--- devices/ellaca/options.nix
+++ devices/ellaca/options.nix
@@ -5,7 +5,7 @@
     mail.enable = true; # broken
     website.enable = false; # broken
     fedi.enable = false; # broken
-    matrix.enable = true; # broken
+    matrix.enable = true;
     git.enable = true;
 
     stateVersion = "25.05";
