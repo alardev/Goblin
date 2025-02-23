010165d1e56ef3767d87fcb8dbf619d53a455038
twoneis <sanjay29@sapura.de>
2025-02-23 02:49:49 +0100

    try this

--- modules/fedi/default.nix
+++ modules/fedi/default.nix
@@ -2,4 +2,29 @@
   inherit (lib) mkIf;
   inherit (config) conf;
 in mkIf conf.fedi.enable {
+  services.akkoma = {
+    enable = true;
+    config = {
+      ":pleroma" = {
+        ":instance" = {
+          name = "miras fedi";
+          description = "miras akkoma instance";
+          email = "mira.cp.0909@gmail.com";
+          registration_open = false;
+        };
+
+        "Pleroma.Web.Endpoint" = {
+          url.host = "fedi.twoneis.site";
+        };
+
+        "Pleroma.Upload" = {
+          base_url = "https://fedi.twoneis.site/media/";
+        };
+      };
+    };
+    nginx = {
+      useACMEHost = "twoneis.site";
+      forceSSL = true;
+    };
+  };
 }
