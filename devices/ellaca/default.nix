16403fc68d33a9b53959ef8a05a8a8afe32eeb8f
twoneis <git@chpu.eu>
2025-03-03 15:06:27 +0100

    hardened kernel

--- devices/ellaca/default.nix
+++ devices/ellaca/default.nix
@@ -13,7 +13,7 @@
 
   boot = {
     initrd.availableKernelModules = ["ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "sd_mod"];
-    kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.linuxPackages_latest_hardened;
     loader = {
       grub = {
         devices = ["/dev/sda"];
