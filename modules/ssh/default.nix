c53a8489195c9dd51bf7fdd8609068b3b8bea86a
twoneis <git@chpu.eu>
2025-03-03 15:08:15 +0100

    no ssh for root

--- modules/ssh/default.nix
+++ modules/ssh/default.nix
@@ -14,10 +14,6 @@ in
       };
     };
 
-    users.users.root.openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMYy89cBNXjet2kBbOw7CKMJguyIq72EQV8ixo836nOH"
-    ];
-
     users.users.${conf.username}.openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMYy89cBNXjet2kBbOw7CKMJguyIq72EQV8ixo836nOH"
     ];
