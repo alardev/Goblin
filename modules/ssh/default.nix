27dd02777d0bbe3e693d62c0b6b197e273c8d6a4
twoneis <git@chpu.eu>
2025-03-06 16:48:01 +0100

    added backup ssh key

--- modules/ssh/default.nix
+++ modules/ssh/default.nix
@@ -15,7 +15,7 @@ in
     };
 
     users.users.${conf.username}.openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMYy89cBNXjet2kBbOw7CKMJguyIq72EQV8ixo836nOH"
       "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAINKg0qThAxqNZjEVhXQ+ds7/CpMAeF1AKsVfQUW8CbtqAAAABHNzaDo="
+      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIE+6ILh1iUkD8FSWlmd2MCH+il8wOXbkkl7U01z/pZQfAAAABHNzaDo="
     ];
   }
